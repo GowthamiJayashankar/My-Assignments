194806a00d26954336ece6736c3a63f8b3772b71
Gowthami <gowthujayashankar@gmail.com>
2026-07-09 14:05:48 +0530

    Add files via upload
    Datatypes

---
+++ DatatypesTest1.js
@@ -0,0 +1,28 @@
+/*  Create the following variables using let (not using var) and check their typeOf
+ a) firstName
+ b) companyName
+ c) mobileNumber
+ d) isAutomation
+ e) hasPlaywright (do not assign)
+Print and confirm the values and data types
+ */
+
+var firstName = "Gowthami";
+console.log(firstName);
+console.log(typeof firstName);
+
+var companyName = "TCS";
+console.log(companyName);
+console.log(typeof companyName);
+
+var mobileNumber = 1234567890;
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+var isAutomation = true;;
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+var hasPlaywright;
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
